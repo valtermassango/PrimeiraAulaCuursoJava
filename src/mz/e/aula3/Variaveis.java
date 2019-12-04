0a1c5ae55655fcd448dfb2ec5202938d6dd2cdc1
valtermassango <58469276+valtermassango@users.noreply.github.com>
2019-12-04 21:54:33 +0200

    Aula 3 Operadores

---
+++ src/mz/e/aula3/Variaveis.java
@@ -0,0 +1,33 @@
+
+package mz.e.aula3;
+
+/**
+ *
+ * @author Valter Massango
+ * @author Jose Seie
+ * @author Filipe Mangue   
+ * @author José Romão
+ * @author Aly Macome
+ * @author Abdul Rasac
+ * @author Adrian Uaeca
+ * @author Efraime Mutole
+ */
+public class Variaveis {
+    public static void main (String[] args) {
+    //variaveis inteiras
+    int idade =10;
+    int tamanho;
+    
+    //variaveis double ou decimais
+    double preco = 10.5;
+    double temperatura;
+    
+    //variaveis que contem apenas um caracter
+    char sexo ='M';
+    
+    //
+    String sexoStr ="";
+    
+    }
+    
+}
