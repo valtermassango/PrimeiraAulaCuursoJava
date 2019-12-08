3905c7fc99d1c70e973526e7f98a08e8a469447b
valtermassango <58469276+valtermassango@users.noreply.github.com>
2019-12-08 22:03:18 +0200

    Aula 5 - Olá Mundo

---
+++ src/mz/e/aula5/OlaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mz.e.aula5;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Valter Massango
+ */
+public class OlaMundo {
+    public static void main (String[]args){
+        System.out.println("Olá Mundo");
+        JOptionPane.showMessageDialog(null,"<html>Código de acesso numero 939<br><br>Seja bem vindo ao curso Básico Intensivo</html>");
+    }
+    
+    
+}
