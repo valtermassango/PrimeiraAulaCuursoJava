b847c0da2434064a25e40f8d1cd0c6d48220af82
valtermassango <58469276+valtermassango@users.noreply.github.com>
2019-12-08 22:29:15 +0200

    Aula 5 - Exercício 2

---
+++ src/mz/e/aula5/Exercicio2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mz.e.aula5;
+
+/**
+ *
+ * @author Curso de JAVA
+ */
+public class Exercicio2 {
+    public static void main (String[]args){
+        System.out.println("Seja bem vindo ao meu programa!");
+        System.out.println("O produto de 8 por 90 é igual a "+90*8);
+        System.out.println("O quociente de 90 por 30 é igual a "+90/30);
+        System.out.println("O quadrado de 9 é igual a "+9*9);
+    }
+    
+}
