6ef2b2113d6c00cbbc46bacefd2a04975964326c
valtermassango <58469276+valtermassango@users.noreply.github.com>
2019-12-08 23:17:28 +0200

    Aula 5 - Exercício 4

---
+++ src/mz/e/aula5/Exercicio4.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mz.e.aula5;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Valter Massango
+ */
+public class Exercicio4 {
+      public static void main (String[]args){
+        //declaração de variáveis
+        String nome;
+        
+         //Entrada de dados
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite seu nome:");
+        nome = entrada.next();
+        
+        JOptionPane.showMessageDialog(null, nome);
+      }
+    }
+
